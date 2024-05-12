5a8503871061593378f92091698fe899145ee957
Alexey Yankov <yankovav.wm@gmail.com>
2024-05-12 18:49:36 +0300

    image fix

--- src/widgets/avatarAnimation/avatarAnimation.tsx
+++ src/widgets/avatarAnimation/avatarAnimation.tsx
@@ -5,7 +5,7 @@ import AvatarPhoto from '/public/avatar/AboutPhoto.png';
 export const AvatarAnimation = () => {
   return (
     <div className="gradient-border" id="box">
-      <Image src={AvatarPhoto} alt="Alexey_photo" height={0} width={0} />
+      <Image src={AvatarPhoto} alt="Alexey_photo" height={0} width={0} priority={true}/>
     </div>
   );
 };
