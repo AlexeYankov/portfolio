4db3cf1754a6753b4a8f1243357e816191df9320
Alexey Yankov <yankovav.wm@gmail.com>
2024-05-25 16:37:10 +0300

    keywords meta added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from 'next';
-import BaseRootLayout from '@/widgets/layout/baseLayout';
 import { Exo } from 'next/font/google';
+import BaseRootLayout from '@/widgets/layout/baseLayout';
 import './globals.css';
-
-type Props = {
-  description: string;
-  title: string;
-};
 
 const exo2 = Exo({
   subsets: ['latin'],
@@ -15,16 +10,17 @@ const exo2 = Exo({
   variable: '--font-exo2',
 });
 
+const keywords = 'react, frontend, фронтенд, typescript, javascript, node, next js, tailwind, redux, react query, веб разработка, веб разрабочик'
+
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: 'Alexey Iankov',
     description: 'Frontend-dev',
-    // metadataBase: new URL('https://1111.com'),
-    // manifest: 'https://iankov.ru',
     category: 'веб разработчик, web developer, frontend',
     icons: {
       icon: '/favicon/favicon.ico', // /public path
     },
+    keywords: keywords,
     openGraph: {
       title: 'Alexey Iankov',
       description: 'Frontend-dev',
@@ -39,9 +35,6 @@ export async function generateMetadata(): Promise<Metadata> {
         index: true,
         follow: false,
         noimageindex: true,
-        // 'max-video-preview': -1,
-        // 'max-image-preview': 'large',
-        // 'max-snippet': -1,
       },
     },
   };
@@ -53,11 +46,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <html lang="en" className={`${exo2.variable} ${roboto_mono.variable}`}>
     <html lang="en" className={`${exo2.variable} scroll-smooth`}>
-      {/* <body className={inter.className}>{children}</body> */}
-      {/* <base href='https://1111.com'/>
-      <meta httpEquiv="X-UA-Compatible" content='IE=edge'/> */}
       <body>
         <BaseRootLayout>{children}</BaseRootLayout>
       </body>
